bfb7e1f2dc2f9eac2079a6ca1d6b8c963a8d3701
Anuj <anuj_sharma@thbs.india.com>
2020-01-10 11:50:42 +0530

    added sanitizer and method override and the new edit view and finalized the app

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express     =   require('express'),
     mongoose    =   require('mongoose'),
-    bodyParser  =   require('body-parser');
+    bodyParser  =   require('body-parser'),
+    methodOverride = require('method-override'),
+    expressSanitizer= require('express-sanitizer');
 
 var app=express();
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(expressSanitizer());
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 
 
@@ -56,6 +60,10 @@ app.get('/blog/new',function(req,res){
 // set the CREATE route here
 app.post('/blogs',function(req,res){
 
+    console.log(req.body);
+    req.body.blog.body=req.sanitize(req.body.blog.body);
+    console.log(req.body);
+
     // create the blog
     Blog.create(req.body.blog,function(err,newBlog){
 
@@ -83,9 +91,59 @@ app.get('/blogs/:id',function(req,res){
 });
 
 
+// EDIT route
+
+app.get('/blogs/:id/edit',function(req,res){
+
+    Blog.findById(req.params.id,function(err,foundBlog){
+
+        if(err){
+            res.redirect('/blogs');
+        } else{
+
+            res.render("edit",{blog : foundBlog});
+        }
+    });
+
+});
 
 
+// UPDATE ROUTE
+app.put('/blogs/:id',function(req,res){
 
-app.listen(3000,function(){
+    req.body.blog.body=req.sanitize(req.body.blog.body);
+
+    Blog.findByIdAndUpdate(req.params.id,req.body.blog,function(err,updatedBlog){
+
+        if(err) res.redirect('/blogs');
+
+        else{
+            res.redirect("/blogs/"+req.params.id);
+        }
+    })
+
+});
+
+
+// DELETE ROUTE
+
+app.delete("/blogs/:id",function(req,res){
+
+    // Destroy blog 
+
+    Blog.findByIdAndRemove(req.params.id,function(err,deltedBlog){
+
+        if(err) res.redirect('/blogs');
+        else{
+            res.redirect('/blogs');
+        }      
+    });
+
+
+    // redirect somewhere
+});
+
+
+app.listen(3000,function(){ 
     console.log("Blog Server Started");
 });
